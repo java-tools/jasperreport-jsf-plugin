142341c5bf61cde691b865a8738f6526395d4478
alonsoft <alonsoft@7ab5314f-46c3-4173-b991-7f4702c40ae5>
2010-03-11 20:56:30 +0000

    Replacing useless samples by sample use cases

---
+++ trunk/samples/jrjsf-usecases/src/main/java/net/sf/jasperreports/jsf/sample/simple/BooksReportBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sf.jasperreports.jsf.sample.simple;
+
+/**
+ *
+ * @author aalonsodominguez
+ */
+public class BooksReportBean {
+
+    private int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+}
