2b3471f063da6b89daadef46480091016d464c60
alonsoft <alonsoft@7ab5314f-46c3-4173-b991-7f4702c40ae5>
2010-09-28 21:12:06 +0000

    Missed files from previous commit

---
+++ trunk/plugin/src/test/java/net/sf/jasperreports/jsf/engine/export/DefaultExporterTest.java
@@ -0,0 +1,213 @@
+/*
+ * JaspertReports JSF Plugin Copyright (C) 2010 A. Alonso Dominguez
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. This library is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA A.
+ *
+ * Alonso Dominguez
+ * alonsoft@users.sf.net
+ */
+package net.sf.jasperreports.jsf.engine.export;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.faces.context.FacesContext;
+import net.sf.jasperreports.engine.JRException;
+
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.jsf.Constants;
+import net.sf.jasperreports.jsf.component.UIReport;
+import net.sf.jasperreports.jsf.engine.ExporterException;
+import net.sf.jasperreports.jsf.engine.fill.DefaultFiller;
+import net.sf.jasperreports.jsf.test.JMockTheories;
+import net.sf.jasperreports.jsf.test.mock.MockFacesEnvironment;
+import net.sf.jasperreports.jsf.test.mock.MockFacesServletEnvironment;
+import org.apache.shale.test.mock.MockServletContext;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author aalonsodominguez
+ */
+@RunWith(JMock.class)
+public class DefaultExporterTest {
+
+    public static final String REPORT_ID = "reportId";
+
+    public static final String CUSTOM_ENCODING = "UTF-8";
+
+    public static final String CUSTOM_JASPER_PRINT_NAME =
+            DefaultExporterTest.class.getName();
+
+    private Mockery mockery = new JUnit4Mockery() {{
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }};
+
+    private MockFacesEnvironment facesEnv;
+    private JRExporter mockExporter;
+    private UIReport report;
+    private OutputStream stream;
+    private JasperPrint print;
+
+    @Before
+    public void init() {
+        facesEnv = new MockFacesServletEnvironment();
+
+        report = new UIReport();
+        report.setId(REPORT_ID);
+
+        mockExporter = mockery.mock(JRExporter.class);
+        stream = new ByteArrayOutputStream();
+        print = mockery.mock(JasperPrint.class);
+    }
+
+    @After
+    public void dispose() {
+        print = null;
+        try {
+            stream.close();
+            stream = null;
+        } catch (IOException e) { }
+
+        report = null;
+        facesEnv.release();
+        facesEnv = null;
+    }
+
+    @Test
+    public void withoutJasperPrintThrowEx() {
+        DefaultExporter exporter = new MockDefaultExporter();
+        try {
+            exporter.export(facesEnv.getFacesContext(), report, stream);
+        } catch (Exception e) {
+            assertThat(e, is(JasperPrintNotFoundException.class));
+
+            String clientId = report.getClientId(facesEnv.getFacesContext());
+            JasperPrintNotFoundException jpnfe =
+                    (JasperPrintNotFoundException) e;
+            assertThat(jpnfe.getClientId(), equalTo(clientId));
+        }
+    }
+
+    @Test
+    public void usingPrintInitsJRExporter() throws Exception {
+        FacesContext context = facesEnv.getFacesContext();
+        
+        context.getExternalContext().getRequestMap().put(
+                DefaultFiller.ATTR_JASPER_PRINT, print);
+
+        mockery.checking(new Expectations() {{
+            atLeast(5).of(mockExporter).setParameter(
+                    with(any(JRExporterParameter.class)),
+                    with(any(Object.class)));
+            oneOf(mockExporter).exportReport();
+        }});
+
+        DefaultExporter exporter = new MockDefaultExporter();
+        exporter.export(context, report, stream);
+    }
+
+    @Test
+    public void useCustomJasperPrintNameAndEncoding() throws Exception {
+        FacesContext context = facesEnv.getFacesContext();
+        MockServletContext servletContext =
+                ((MockFacesServletEnvironment) facesEnv).getServletContext();
+        servletContext.addInitParameter(
+                Constants.JASPER_PRINT_ATTR_NAME, CUSTOM_JASPER_PRINT_NAME);
+
+        context.getExternalContext().getRequestMap().put(
+                CUSTOM_JASPER_PRINT_NAME, print);
+        report.getAttributes().put(
+                DefaultExporter.ATTR_CHARACTER_ENCODING, CUSTOM_ENCODING);
+
+        mockery.checking(new Expectations() {{
+            atLeast(5).of(mockExporter).setParameter(
+                    with(any(JRExporterParameter.class)),
+                    with(any(Object.class)));
+            /*oneOf(mockExporter).setParameter(
+                    JRExporterParameter.CHARACTER_ENCODING, CUSTOM_ENCODING);
+            atLeast(1).of(mockExporter).setParameter(
+                    with(any(JRExporterParameter.class)),
+                    with(any(Object.class)));*/
+            oneOf(mockExporter).exportReport();
+        }});
+
+        DefaultExporter exporter = new MockDefaultExporter();
+        exporter.export(context, report, stream);
+    }
+
+    @Test
+    public void withJRExRethrowItWrapped() throws Exception {
+        FacesContext context = facesEnv.getFacesContext();
+        String reportId = report.getClientId(context);
+        context.getExternalContext().getRequestMap().put(
+                DefaultFiller.ATTR_JASPER_PRINT, print);
+
+        final JRException jrException = new JRException(reportId);
+
+        mockery.checking(new Expectations() {{
+            atLeast(5).of(mockExporter).setParameter(
+                    with(any(JRExporterParameter.class)),
+                    with(any(Object.class)));
+            oneOf(mockExporter).exportReport();
+            will(throwException(jrException));
+        }});
+
+        DefaultExporter exporter = new MockDefaultExporter();
+        try {
+            exporter.export(context, report, stream);
+        } catch (Exception e) {
+            assertThat(e, is(ExporterException.class));
+            assertThat(e.getCause(), notNullValue());
+            assertThat(e.getCause(), is(JRException.class));
+        }
+    }
+    
+    private class MockDefaultExporter extends DefaultExporter {
+
+        private String contentType;
+
+        @Override
+        protected JRExporter createJRExporter(FacesContext context, 
+                UIReport component)
+        throws ExporterException {
+            return mockExporter;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public void setContentType(String contentType) {
+            this.contentType = contentType;
+        }
+
+    }
+
+}
---
+++ trunk/plugin/src/test/java/net/sf/jasperreports/jsf/test/mock/MockExternalContextHelper.java
@@ -0,0 +1,70 @@
+/*
+ * JaspertReports JSF Plugin Copyright (C) 2010 A. Alonso Dominguez
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. This library is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA A.
+ *
+ * Alonso Dominguez
+ * alonsoft@users.sf.net
+ */
+package net.sf.jasperreports.jsf.test.mock;
+
+import java.io.IOException;
+import javax.faces.context.ExternalContext;
+import net.sf.jasperreports.jsf.component.UIReport;
+import net.sf.jasperreports.jsf.context.ExternalContextHelper;
+import net.sf.jasperreports.jsf.engine.ReportRenderRequest;
+import net.sf.jasperreports.jsf.renderkit.ReportRenderer;
+
+/**
+ *
+ * @author aalonsodominguez
+ */
+public class MockExternalContextHelper extends ExternalContextHelper {
+
+    private String requestServerName;
+
+    @Override
+    public ReportRenderRequest restoreReportRequest(ExternalContext context) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getRequestURI(ExternalContext context) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getRequestServerName(ExternalContext context) {
+        return requestServerName;
+    }
+
+    public void setRequestServerName(String requestServerName) {
+        this.requestServerName = requestServerName;
+    }
+
+    @Override
+    public String getResourceRealPath(ExternalContext context, String name) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void writeHeaders(ExternalContext context, ReportRenderer renderer, UIReport report) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void writeResponse(ExternalContext context, String contentType, byte[] data) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
