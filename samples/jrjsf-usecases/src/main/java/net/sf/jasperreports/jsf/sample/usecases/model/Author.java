9f0eb8011102c4f1218d8bf113ed33d428a5c3d2
alonsoft <alonsoft@users.noreply.github.com>
2011-07-06 16:40:33 +0000

    Developing a proper sample application

---
+++ samples/jrjsf-usecases/src/main/java/net/sf/jasperreports/jsf/sample/usecases/model/Author.java
@@ -0,0 +1,43 @@
+/*
+ * JaspertReports JSF Plugin Copyright (C) 2011 A. Alonso Dominguez
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. This library is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA A.
+ *
+ * Alonso Dominguez
+ * alonsoft@users.sf.net
+ */
+package net.sf.jasperreports.jsf.sample.usecases.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author 501944227
+ */
+@Entity
+public class Author {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String name;
+    
+    @OneToMany
+    private List<Book> books;
+    
+}
